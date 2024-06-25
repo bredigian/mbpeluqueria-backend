72010e243579bfceaa0de2c5359f7b2bf61b9e18
Gianluca Bredice <bredicegian@gmail.com>
2024-06-25 11:17:57 -0300

    fix: verify if workhour for add exists before add it

--- src/controllers/workhours.controller.ts
+++ src/controllers/workhours.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { create, getAll } from "../services/workhours.service"
+import { create, getAll, getOne } from "../services/workhours.service"
 
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 import { Workhour } from "@prisma/client"
@@ -22,6 +22,18 @@ export const Controller = {
   create: async (req: Request, res: Response) => {
     try {
       const payload: Workhour = req.body
+      const exists = await getOne(payload)
+
+      if (exists)
+        return res.status(409).json({
+          message: `El horario ${payload.hours
+            .toString()
+            .padStart(2, "0")}:${payload.minutes
+            .toString()
+            .padStart(2, "0")} ya existe.`,
+          name: "Conflict",
+          statusCode: 409,
+        })
 
       return res.status(201).json(await create(payload))
     } catch (error) {
