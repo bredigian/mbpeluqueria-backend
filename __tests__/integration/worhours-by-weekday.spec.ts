e240a76af103cbab185a46274f19ad08e52a69b7
Gianluca Bredice <bredicegian@gmail.com>
2024-09-03 10:48:45 -0300

    feature: add workhours-by-weekday integration tests and change users.spec to auth.spec tests

---
+++ __tests__/integration/worhours-by-weekday.spec.ts
@@ -0,0 +1,77 @@
+import { $connect } from "../../src/services/prisma.service"
+import App from "../../src/app"
+import TestAgent from "supertest/lib/agent"
+import { WorkhoursByWeekday } from "@prisma/client"
+import express from "express"
+import request from "supertest"
+import { workhourIsEnabled } from "../../src/services/workhours-by-weekdays.service"
+
+describe("Workhours by weekday Integration Tests", () => {
+  const WORKHOUR_TO_HANDLE: Partial<WorkhoursByWeekday> = {
+    weekday_id: "clxz0hhyh0003fmlb4uay1mj1", // Miércoles
+    workhour_id: "clxz0ils30007fmlbe2pxmnyk", // 10:00hs
+  }
+
+  const AUTHORIZATION =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNtMGxteXo5dTAwMDBqNXF3bDd0N2s5cngiLCJuYW1lIjoiTm9tYnJlIEFwZWxsaWRvIiwicGhvbmVfbnVtYmVyIjoiMTIzNDk4NzY1NCIsImVtYWlsIjoidGVzdGluZ0B0ZXN0LmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzI1MzE5OTgzLCJleHAiOjE3Mjc5MTE5ODN9.nJymSjmfZxDHkvsN4qTl7pxdyOoheQ0Xld5h0mNnwco"
+
+  let app: TestAgent
+  beforeAll(async () => {
+    await $connect()
+    const server = App(express())
+    app = request(server)
+  })
+
+  it("should activate workhour and return 201", async () => {
+    const response = await app
+      .post("/workhours-by-weekday")
+      .set({ authorization: `Bearer ${AUTHORIZATION}` })
+      .send(WORKHOUR_TO_HANDLE)
+
+    const isEnabled = await workhourIsEnabled(
+      WORKHOUR_TO_HANDLE.weekday_id as string,
+      WORKHOUR_TO_HANDLE.workhour_id as string
+    )
+
+    expect(isEnabled).toBeTruthy() // Verifica que realmente se habilitó
+
+    expect(response.status).toBe(201)
+    expect(response.body).toEqual(expect.objectContaining(WORKHOUR_TO_HANDLE))
+  })
+
+  it("should deactivate workhour and return 200", async () => {
+    const response = await app
+      .post("/workhours-by-weekday")
+      .set({ authorization: `Bearer ${AUTHORIZATION}` })
+      .send(WORKHOUR_TO_HANDLE)
+
+    const isEnabled = await workhourIsEnabled(
+      WORKHOUR_TO_HANDLE.weekday_id as string,
+      WORKHOUR_TO_HANDLE.workhour_id as string
+    )
+
+    expect(isEnabled).toBeFalsy()
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(expect.objectContaining({ count: 1 }))
+  })
+
+  it("should fail and return 500", async () => {
+    const WORKHOUR_TO_HANDLE: Partial<WorkhoursByWeekday> = {
+      weekday_id: "clxz0hhyh0003fmlb4uay", // ID Inválido
+      workhour_id: "clxz0ils30007fmlbe2px", // ID Inválido
+    }
+    const response = await app
+      .post("/workhours-by-weekday")
+      .set({ authorization: `Bearer ${AUTHORIZATION}` })
+      .send(WORKHOUR_TO_HANDLE)
+
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        name: "PrismaClientKnownRequestError",
+        statusCode: "P2003",
+      })
+    )
+  })
+})
